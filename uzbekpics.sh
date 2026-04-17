76f1c493e11f43304e028606b2102aa5d1d1193e
Parsast <parsastdev@duck.com>
2026-04-17 15:18:10 +0330

    add danbooru support

--- uzbekpics.sh
+++ uzbekpics.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# uzbekpics.sh: Scrapes images at yande.re,konachan.com
+# uzbekpics.sh: Scrapes images at yande.re,konachan.com,danbooru.donmai.us
 
 #read -p "Enter the tag: " TAG
 
 HARDCODED_RATING=+-rating%3Asafe
+HARDCODED_RATING_DANBOORU=+-rating%3Ageneral
 YANDERE_URL="https://yande.re"
 KONACHAN_URL="https://konachan.com"
+DANBOORU_API="https://danbooru.donmai.us/posts.json?tags="
 API_QUERY="/post.json?tags="
 
 if [ -z "$1" ]; then
@@ -26,6 +28,7 @@ TAG=$1
 # Create directory for the images
 mkdir -p "yandere_$TAG"
 mkdir -p "konachan_$TAG"
+mkdir -p "danbooru_$TAG"
 
 # Download the image
-curl -s "${YANDERE_URL}${API_QUERY}${TAG}${HARDCODED_RATING}" | jq -r '.[].jpeg_url' | aria2c -i- -d "yandere_$TAG" && curl -s "${KONACHAN_URL}${API_QUERY}${TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "konachan_$TAG"
+curl -s "${YANDERE_URL}${API_QUERY}${TAG}${HARDCODED_RATING}" | jq -r '.[].jpeg_url' | aria2c -i- -d "yandere_$TAG" && curl -s "${KONACHAN_URL}${API_QUERY}${TAG}${HARDCODED_RATING}" | jq -r '.[].jpeg_url' | aria2c -i- -d "konachan_$TAG" && curl -s "${DANBOORU_API}${TAG}${HARDCODED_RATING_DANBOORU}" | jq -r '.[].url' | aria2c -i- -d "danbooru_$TAG"
